fa01c5338a4b276b9e7261113123bb5271905964
bbalkon <bbalkonsky@gmail.com>
2019-12-04 22:06:57 +0500

    second get

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ let db = new sqlite3.Database('src/database.db', sqlite3.OPEN_READONLY, (err) =>
     console.log('Connected to the database.');
 });
 
+app.get("/project/:id", function(request, response){
+    const sql = `SELECT * FROM projects WHERE id = ?`;
+    const id = request.params["id"];
+
+    db.serialize(() => {
+        db.all(sql, id, (err, result) => {
+            if (err) {
+                console.error(err.message);
+            }
+            response.json(result);
+        });
+    });
+});
+
 app.get("/all", function(request, response){
     const sql = `SELECT * FROM projects`;
 
@@ -24,6 +38,14 @@ app.get("/all", function(request, response){
         });
     });
 });
+
+// db.run(`INSERT INTO projects VALUES(?)`, ['C'], function(err) {
+//     if (err) {
+//         return console.log(err.message);
+//     }
+//     // get the last insert id
+//     console.log(`A row has been inserted with rowid ${this.lastID}`);
+// });
 
 const conn = app.listen(4000, '0.0.0.0');
 
